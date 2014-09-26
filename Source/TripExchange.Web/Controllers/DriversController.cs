a53cf4cb183f6334ef036e3127ccb75ac955b93f
NikolayIT <admin@nikolay.it>
2014-09-26 19:02:15 +0300

    Started working on DriversController

--- Source/TripExchange.Web/Controllers/DriversController.cs
+++ Source/TripExchange.Web/Controllers/DriversController.cs
@@ -1,6 +1,11 @@
 ﻿namespace TripExchange.Web.Controllers
 {
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Web.Http;
+
     using TripExchange.Data;
+    using TripExchange.Web.Models.Drivers;
 
     public class DriversController : BaseApiController
     {
@@ -12,6 +17,29 @@
         public DriversController(ITripExchangeData data)
             : base(data)
         {
+        }
+
+        [HttpGet]
+        public IEnumerable<DriverViewModel> Get()
+        {
+            var query = this.Data.Users.All().Where(x => x.IsDriver);
+
+            var data = query.Select(user => new DriverViewModel
+                                                {
+                                                    Id = user.Id,
+                                                    Name = user.UserName,
+                                                    NumberOfTotalTrips = 0, // TODO: Implement
+                                                    NumberOfUpcomingTrips = 0, // TODO: Implement
+                                                });
+            data = data.OrderByDescending(driver => driver.NumberOfTotalTrips).ThenBy(driver => driver.Name);
+            return data.ToList();
+        }
+
+        [HttpGet]
+        [Authorize]
+        public DriverViewModel Get(string driverId)
+        {
+            return null;
         }
     }
 }
---
+++ Source/TripExchange.Web/Models/Drivers/DriverViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace TripExchange.Web.Models.Drivers
+{
+    public class DriverViewModel
+    {
+        public string Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int NumberOfUpcomingTrips { get; set; }
+
+        public int NumberOfTotalTrips { get; set; }
+    }
+}
