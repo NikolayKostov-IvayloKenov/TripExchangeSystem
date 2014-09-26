d7e8d4509415d28599237b3e2a30c4304a806999
NikolayIT <admin@nikolay.it>
2014-09-26 12:36:40 +0300

    Refactorings and StyleCop warnings fixed

--- Source/TripExchange.Web/App_Start/FilterConfig.cs
+++ Source/TripExchange.Web/App_Start/FilterConfig.cs
@@ -1,8 +1,7 @@
-﻿using System.Web;
-using System.Web.Mvc;
-
-namespace TripExchange.Web
+﻿namespace TripExchange.Web
 {
+    using System.Web.Mvc;
+
     public class FilterConfig
     {
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
--- Source/TripExchange.Web/App_Start/WebApiConfig.cs
+++ Source/TripExchange.Web/App_Start/WebApiConfig.cs
@@ -1,14 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net.Http;
-using System.Web.Http;
-using Microsoft.Owin.Security.OAuth;
-using Newtonsoft.Json.Serialization;
-
-namespace TripExchange.Web
+﻿namespace TripExchange.Web
 {
+    using System.Web.Http;
     using System.Web.Http.Cors;
+
+    using Microsoft.Owin.Security.OAuth;
 
     public static class WebApiConfig
     {
