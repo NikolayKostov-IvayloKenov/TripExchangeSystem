2a3bef601f5c0ff663851855ee97e57ce9c17996
NikolayIT <admin@nikolay.it>
2014-09-26 12:34:18 +0300

    Password requirements removed

--- Source/TripExchange.Web/App_Start/WebApiConfig.cs
+++ Source/TripExchange.Web/App_Start/WebApiConfig.cs
@@ -28,8 +28,7 @@ namespace TripExchange.Web
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
                 routeTemplate: "api/{controller}/{id}",
-                defaults: new { id = RouteParameter.Optional }
-            );
+                defaults: new { id = RouteParameter.Optional });
         }
     }
 }
