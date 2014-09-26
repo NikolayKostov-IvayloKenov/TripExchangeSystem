cf6c5182f8d3bffa4280968858abd0c14c06981c
NikolayIT <admin@nikolay.it>
2014-09-26 17:04:20 +0300

    Implemented repository pattern and unit of work pattern

---
+++ Source/TripExchange.Data/ITripExchangeData.cs
@@ -0,0 +1,13 @@
+﻿namespace TripExchange.Data
+{
+    using TripExchange.Models;
+
+    public interface ITripExchangeData
+    {
+        IRepository<ApplicationUser> Users { get; }
+
+        IRepository<Trip> Trips { get; }
+
+        int SaveChanges();
+    }
+}
