3f1a54936720f03011bca49cdbb59ec9df07ccb9
NikolayIT <admin@nikolay.it>
2014-09-26 12:00:18 +0300

    Installed CORS for the WebAPI authentication

--- Source/TripExchange.Web/App_Start/Startup.Auth.cs
+++ Source/TripExchange.Web/App_Start/Startup.Auth.cs
@@ -5,45 +5,49 @@ using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
 using Microsoft.Owin;
 using Microsoft.Owin.Security.Cookies;
-using Microsoft.Owin.Security.Google;
 using Microsoft.Owin.Security.OAuth;
 using Owin;
-using TripExchange.Web.Providers;
-using TripExchange.Web.Models;
+using CorsApi.Providers;
 
-namespace TripExchange.Web
+namespace CorsApi
 {
     public partial class Startup
     {
+        static Startup()
+        {
+            PublicClientId = "self";
+
+            UserManagerFactory = () => new UserManager<IdentityUser>(new UserStore<IdentityUser>());
+
+            OAuthOptions = new OAuthAuthorizationServerOptions
+            {
+                TokenEndpointPath = new PathString("/Token"),
+                Provider = new ApplicationOAuthProvider(PublicClientId, UserManagerFactory),
+                AuthorizeEndpointPath = new PathString("/api/Account/ExternalLogin"),
+                AccessTokenExpireTimeSpan = TimeSpan.FromDays(14),
+                AllowInsecureHttp = true
+            };
+        }
+
         public static OAuthAuthorizationServerOptions OAuthOptions { get; private set; }
+
+        public static Func<UserManager<IdentityUser>> UserManagerFactory { get; set; }
 
         public static string PublicClientId { get; private set; }
 
         // For more information on configuring authentication, please visit http://go.microsoft.com/fwlink/?LinkId=301864
         public void ConfigureAuth(IAppBuilder app)
         {
-            // Configure the db context and user manager to use a single instance per request
-            app.CreatePerOwinContext(ApplicationDbContext.Create);
-            app.CreatePerOwinContext<ApplicationUserManager>(ApplicationUserManager.Create);
-
             // Enable the application to use a cookie to store information for the signed in user
             // and to use a cookie to temporarily store information about a user logging in with a third party login provider
             app.UseCookieAuthentication(new CookieAuthenticationOptions());
             app.UseExternalSignInCookie(DefaultAuthenticationTypes.ExternalCookie);
 
-            // Configure the application for OAuth based flow
-            PublicClientId = "self";
-            OAuthOptions = new OAuthAuthorizationServerOptions
-            {
-                TokenEndpointPath = new PathString("/Token"),
-                Provider = new ApplicationOAuthProvider(PublicClientId),
-                AuthorizeEndpointPath = new PathString("/api/Account/ExternalLogin"),
-                AccessTokenExpireTimeSpan = TimeSpan.FromDays(14),
-                AllowInsecureHttp = true
-            };
-
             // Enable the application to use bearer tokens to authenticate users
             app.UseOAuthBearerTokens(OAuthOptions);
+
+            // Enable CORS
+            app.UseCors(Microsoft.Owin.Cors.CorsOptions.AllowAll);
 
             // Uncomment the following lines to enable logging in with third party login providers
             //app.UseMicrosoftAccountAuthentication(
@@ -58,11 +62,7 @@ namespace TripExchange.Web
             //    appId: "",
             //    appSecret: "");
 
-            //app.UseGoogleAuthentication(new GoogleOAuth2AuthenticationOptions()
-            //{
-            //    ClientId = "",
-            //    ClientSecret = ""
-            //});
+            //app.UseGoogleAuthentication();
         }
     }
 }
--- Source/TripExchange.Web/Startup.cs
+++ Source/TripExchange.Web/Startup.cs
@@ -3,15 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using Microsoft.Owin;
 using Owin;
+using Microsoft.Owin.Cors;
+using System.Web.Cors;
+using System.Threading.Tasks;
 
-[assembly: OwinStartup(typeof(TripExchange.Web.Startup))]
+[assembly: OwinStartup(typeof(CorsApi.Startup))]
 
-namespace TripExchange.Web
+namespace CorsApi
 {
     public partial class Startup
     {
         public void Configuration(IAppBuilder app)
         {
+            app.UseCors(new CorsOptions()
+            {
+                PolicyProvider = new CorsPolicyProvider()
+                {
+                    PolicyResolver = request =>
+                    {
+                        if (request.Path.StartsWithSegments(new PathString("/token")))
+                        {
+                            return Task.FromResult(new CorsPolicy { AllowAnyOrigin = true });
+                        }
+                        return Task.FromResult<CorsPolicy>(null);
+                    }
+                }
+            });
             ConfigureAuth(app);
         }
     }
