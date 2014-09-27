df7f02846d7952d2c4b749a3344c31699c44c9f4
NikolayIT <admin@nikolay.it>
2014-09-27 17:42:36 +0300

    Implemented creating trips with post request to /api/trips

--- Source/TripExchange.Web/Controllers/TripsController.cs
+++ Source/TripExchange.Web/Controllers/TripsController.cs
@@ -1,6 +1,13 @@
 ﻿namespace TripExchange.Web.Controllers
 {
+    using System.Linq;
+    using System.Web.Http;
+
+    using Microsoft.AspNet.Identity;
+
     using TripExchange.Data;
+    using TripExchange.Models;
+    using TripExchange.Web.Models.Trips;
 
     public class TripsController : BaseApiController
     {
@@ -12,6 +19,51 @@
         public TripsController(ITripExchangeData data)
             : base(data)
         {
+        }
+
+        public IHttpActionResult Get(string id)
+        {
+            return null;
+        }
+
+        [HttpPost]
+        public IHttpActionResult Post(CreateTripBindingModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return this.BadRequest(this.ModelState);
+            }
+
+            var fromCity = this.Data.Cities.All().FirstOrDefault(city => city.Name == model.From);
+            if (fromCity == null)
+            {
+                return this.BadRequest("\"From\" city does not exist!");
+            }
+
+            var toCity = this.Data.Cities.All().FirstOrDefault(city => city.Name == model.To);
+            if (toCity == null)
+            {
+                return this.BadRequest("\"To\" city does not exist!");
+            }
+
+            var currentUserId = User.Identity.GetUserId();
+            var currentUser = this.Data.Users.GetById(currentUserId);
+
+            var trip = new Trip
+                           {
+                               AvailableSeats = (byte)(model.AvailableSeats + 1),
+                               From = fromCity,
+                               To = toCity,
+                               DepartureTime = model.DepartureTime,
+                               Driver = currentUser,
+                           };
+
+            trip.Passengers.Add(currentUser);
+
+            this.Data.Trips.Add(trip);
+            this.Data.SaveChanges();
+
+            return this.Get(trip.Id.ToString());
         }
     }
 }
